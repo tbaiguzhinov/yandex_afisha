d3f3512c7becb73e78909eacbb99edf923747e2a
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-07-14 18:28:50 +0600

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
