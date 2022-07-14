31b768182dd1f83f0c44fba219a026dc15a0eebe
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-07-14 18:02:49 +0600

    Add places model and admin

--- places/models.py
+++ places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Place(models.Model):
+    """Модель места."""
+    title = models.CharField("Название", max_length=100)
+    description_short = models.CharField("Короткое описание", max_length=500)
+    description_long = models.TextField("Длинное описание")
+    lng = models.FloatField("Широта")
+    lat = models.FloatField("Долгота")
+
+    def __str__(self):
+        return self.title
