821704f65a54fb4585d97571b1e4d6d4a49a184c
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-07-15 13:07:39 +0600

    Add image upload to places model

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from places.models import Place, Image
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+
+    def get_extra(self, request, obj=None, **kwargs):
+        return 1
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        ImageInline,
+    ]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
