660d7b1a8e134afa3736eb54c52b70e9fcde4f32
tbaiguzhinov <tim.baiguzhinov@gmail.com>
2022-07-15 18:46:26 +0600

    Add preview for images

--- places/admin.py
+++ places/admin.py
@@ -1,18 +1,35 @@
 from django.contrib import admin
 from places.models import Place, Image
+from django.utils.html import format_html
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+
+    readonly_fields = ("image_preview", )
+    
+    fields = ('image', 'image_preview', )
+
+    def image_preview(self, instance):
+        maximum_height = 200
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url = instance.image.url,
+            width = maximum_height*(instance.image.width/instance.image.height),
+            height = maximum_height,
+            )
+        )
 
     def get_extra(self, request, obj=None, **kwargs):
         return 1
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+
+
     inlines = [
         ImageInline,
     ]
+    
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
